cb215c82dcc8324ba383c26659208ae63a7d0c05
Ayush Sharma <ayushvibrant@gmail.com>
2018-01-14 20:13:59 +0530

    UNIX epoch time to usual time format

---
+++ DateFormatter.java
@@ -0,0 +1,17 @@
+// It converts UNIX epoch time format into ususal time format
+
+     /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+ private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
